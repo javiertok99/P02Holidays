f33d85d55b060b580bbc682ff8fe49529f27b6d8
Javier <16022934@myrp.edu.sg>
2018-04-23 11:01:20 +0800

    Commit #2 Done Holiday Adapter

---
+++ app/src/main/java/com/example/a16022934/p02_holidays/Holiday.java
@@ -0,0 +1,27 @@
+package com.example.a16022934.p02_holidays;
+
+public class Holiday {
+    String name;
+    String date;
+
+    public Holiday(String name, String date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
